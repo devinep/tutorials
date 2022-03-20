4bafdece1b2e94aac983faabb43df78411046f16
devinep <paul-devine@outlook.com>
2022-03-20 21:41:28 +0000

    nothing

--- Random Python/historle.py
+++ Random Python/historle.py
@@ -1,19 +1,25 @@
 #data which is event followed by the year
 data = ["Paul Devine's birth", 1983]
 
+print (type(data))
 #Ask for user input and take the first bit of data from list
 
 print("When did this event occur?")
 print(data[0])
 
-guess = input
+guess = int(input())
+tries = 5
 
-if guess == (data[1]):
-    print("That's right")
-elif guess < (data[1]):
-    print("Too low sorry")
-else:
-    print("Too high")
+while guess == True:
+
+    if guess == (data[1]):
+        print("That's right")
+    if guess < (data[1]):
+        print("Too low sorry")
+        continue
+    else:
+        print("Too high")
+        continue
 
 
 #need to tell user if high or low
